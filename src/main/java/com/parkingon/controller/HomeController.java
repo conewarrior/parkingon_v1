34bc915ef3c9547a859a9be9277ab042b05dde20
김한솔 <hskim@gpters.org>
2025-11-10 08:51:28 +0900

    현재 디자인 완성버전

--- src/main/java/com/parkingon/controller/HomeController.java
+++ src/main/java/com/parkingon/controller/HomeController.java
@@ -47,6 +47,22 @@ public class HomeController {
     @GetMapping("/dashboard-worker")
     public String dashboardWorker(HttpSession session, Model model) {
         setSessionInfo(session, model);
+
+        // 더미 아파트 데이터 생성
+        java.util.List<java.util.Map<String, Object>> apartments = new java.util.ArrayList<>();
+        String[] aptNames = {"삼성1단지", "래미안2차", "e편한세상", "힐스테이트", "자이아파트",
+                            "푸르지오", "더샵", "롯데캐슬", "SK뷰", "호반베르디움",
+                            "동아아파트", "현대아파트", "대우아파트", "LG아파트", "GS아파트"};
+
+        for (int i = 0; i < aptNames.length; i++) {
+            java.util.Map<String, Object> apt = new java.util.HashMap<>();
+            apt.put("id", (long)(i + 1));
+            apt.put("code", String.format("A%07d", i + 1));
+            apt.put("name", aptNames[i]);
+            apartments.add(apt);
+        }
+
+        model.addAttribute("apartments", apartments);
         return "dashboard-worker";
     }
 
